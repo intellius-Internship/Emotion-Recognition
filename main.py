c3c2d2b53311aee9a61b51b9efe91d841aa06ea0
aqaqsubin <aqaq925120k@naver.com>
2021-12-30 17:25:11 +0900

    :adhesive_bandage: gpu 할당

--- main.py
+++ main.py
@@ -95,4 +95,5 @@ if __name__ == "__main__":
         logging.info('best model path {}'.format(checkpoint_callback.best_model_path))
 
     else:
-        evaluation(args)
+        with torch.cuda.device(args.gpuid[0]):
+            evaluation(args)
