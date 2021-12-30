73f44d141ce49c0e6dc0153bbb0a438c3ec1050d
aqaqsubin <aqaq925120k@naver.com>
2021-12-30 16:58:09 +0900

    :construction: bigbird, bert 추가

--- utils/model_util.py
+++ utils/model_util.py
@@ -1,11 +1,27 @@
-from transformers import ElectraForSequenceClassification, ElectraTokenizer, ElectraConfig
+from transformers import (ElectraForSequenceClassification, ElectraTokenizer, ElectraConfig,
+                        BertForSequenceClassification, BertConfig,
+                        AutoModelForSequenceClassification, AutoTokenizer, AutoConfig)
 
-def load_model(model_name, num_labels, labels=None):
+from tokenization import KoBertTokenizer
+
+def load_model(model_name, num_labels, labels=None, cache_dir='./cache'):
     if labels is None:
         labels = list(range(num_labels))
 
     if 'bert' == model_name:
-        raise NotImplementedError('Should implement this!')
+        config = BertConfig.from_pretrained(
+            "monologg/kobert",
+            num_labels=num_labels,
+            id2label={str(i): label for i, label in enumerate(labels)},
+            label2id={label: i for i, label in enumerate(labels)}
+        )
+    
+        model = BertForSequenceClassification.from_pretrained(
+            "monologg/kobert", 
+            config=config
+        )
+        tokenizer = KoBertTokenizer.from_pretrained("monologg/kobert")
+        return model, tokenizer
 
     elif 'electra' == model_name:
         config = ElectraConfig.from_pretrained(
@@ -14,17 +30,25 @@ def load_model(model_name, num_labels, labels=None):
             id2label={str(i): label for i, label in enumerate(labels)},
             label2id={label: i for i, label in enumerate(labels)}
         )
-    
         model = ElectraForSequenceClassification.from_pretrained(
             "monologg/koelectra-base-v3-discriminator", 
             config=config
         )
         tokenizer = ElectraTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
-
         return model, tokenizer
 
     elif 'bigbird' == model_name:
-        raise NotImplementedError('Should implement this!')
+        config = AutoConfig.from_pretrained("monologg/kobigbird-bert-base", 
+                num_labels=num_labels,
+                cache_dir=cache_dir)
+        config.label2id = {str(i): label for i, label in enumerate(labels)}
+        config.id2label = {label: i for i, label in enumerate(labels)}
+        model = AutoModelForSequenceClassification.from_pretrained(
+            "monologg/kobigbird-bert-base", 
+            config=config
+        )
+        tokenizer = AutoTokenizer.from_pretrained("monologg/kobigbird-bert-base")
+        return model, tokenizer
 
-
+  
     raise NotImplementedError('Unknown model')
